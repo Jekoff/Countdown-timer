165c8b92d7b7962cd6fa1fc668dcb3a1bb40f22b
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-12-04 20:58:05 +0100

    commit

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 const startingMinutes = 10;
 let time = startingMinutes * 60;
 
-const countdownEl = document.getElementById(countdown);
+// Getting the HTML element with the id "countdown".
+const countdownEl = document.getElementById('countdown');
 
-
+// Setting an interval to call the updateCountdown function every second.
 setInterval(updateCountdown, 1000);
-
+// UpdateCountdown function, to calculate the minutes and seconds
+//left, and update the "countdown" element's innerHTML with the 
+//remaining time.
 function updateCountdown(){
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
